005238f557499a6df550725515d81c202ec5b358
HadyKhaled1 <42691592+HadyKhaled1@users.noreply.github.com>
2018-08-28 08:15:38 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+var prefix = "*"
 client.on('message',async msg => {
      if(msg.channel.type === "dm") return;
   if(msg.author.bot) return;
